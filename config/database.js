02a97c7ef3f07bebbbaa2ab07a41eda00482a7c3
Enzo Figlioli <enzofiglioli.p@gmail.com>
2024-11-01 11:13:40 -0300

    Se agrego dotenv, production, local, y local_railway

--- config/database.js
+++ config/database.js
@@ -1,5 +1,7 @@
-process.loadEnvFile();
 const {Sequelize} = require("sequelize");
+const dotenv = require("dotenv");
+const ENV = process.env.NODE_ENV || 'local';
+dotenv.config({path:`.env.${ENV}`});
 
 const {DATABASE, DBUSER, PASSWORD, HOST} = process.env;
 
