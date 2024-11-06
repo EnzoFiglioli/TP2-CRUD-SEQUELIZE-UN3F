c9ba04bb88c81eaeeeea435ff99cbb7297763b06
enzofiglioli.p <enzofiglioli.p@gmail.com>
2024-11-06 01:14:04 +0000

    Agregue swagger a crear actor

--- controllers/actoresController.js
+++ controllers/actoresController.js
@@ -2,6 +2,8 @@ const Actor = require('../models/actor');
 const crearActor = async(req,res)=> {
     try{
         const {nombre_actor} = req.body;
+        const existe = await Actor.findOne({where:{nombre_actor}})
+        if(existe) return res.status(400).json({msg:"El actor ya existe"}); // busca si ya existe un actor con ese nombre
         const actor = await Actor.create({
             nombre_actor
         });
--- routes/actoresRouter.js
+++ routes/actoresRouter.js
@@ -2,6 +2,62 @@ const express = require('express')
 const router = express.Router();
 const {crearActor} = require("../controllers/actoresController.js")
 
-router.post("/crear",crearActor);
+/**
+ * @swagger
+ * /crear:
+ *   post:
+ *     summary: Crea un nuevo actor
+ *     description: Endpoint para crear un nuevo actor en la base de datos.
+ *     tags: 
+ *       - Actores
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               nombre_actor:
+ *                 type: string
+ *                 description: Nombre del actor
+ *                 example: "Robert Downey Jr."
+ *     responses:
+ *       201:
+ *         description: Actor creado exitosamente
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id_actor:
+ *                   type: integer
+ *                   description: ID del nuevo actor
+ *                   example: 1
+ *                 nombre_actor:
+ *                   type: string
+ *                   description: Nombre del actor
+ *                   example: "Robert Downey Jr."
+ *       400:
+ *         description: Error en la solicitud
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 msg:
+ *                   type: string
+ *                   example: "Faltan datos obligatorios"
+ *       500:
+ *         description: Error interno del servidor
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 msg:
+ *                   type: string
+ *                   example: "Error al crear el actor"
+ */
+router.post("/crear", crearActor);
 
 module.exports = router;
